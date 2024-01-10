6c1571bee726759c1424d1416a915c9d42379c67
dieghino26 <brotzadiego@gmail.com>
2024-01-10 17:11:09 +0100

    added numbers

--- js/campominato.js
+++ js/campominato.js
@@ -6,10 +6,11 @@ const rows = 10
 const cols = 10
 const totalCells = rows * cols
 
-for (let i = 1; i < totalCells; i++) {
+for (let i = 0; i < totalCells; i++) {
     //cella
     const cell = document.createElement("div")
     cell.className = "cell"
+    cell.innerText = i + 1
     //aggiungere in pagina
     grid.appendChild(cell)
 }
