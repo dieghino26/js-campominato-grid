612ecd1a52572b46aa67af6cd35bcfe92be014ea
dieghino26 <brotzadiego@gmail.com>
2024-01-10 17:20:40 +0100

    finisced the program

--- js/campominato.js
+++ js/campominato.js
@@ -1,16 +1,28 @@
 //recuperare la griglia da html
 const grid = document.getElementById("grid")
 
+//recupero il bottone play
+const button = document.getElementById("button")
+
 //rows e cols
 const rows = 10
 const cols = 10
 const totalCells = rows * cols
 
-for (let i = 0; i < totalCells; i++) {
-    //cella
-    const cell = document.createElement("div")
-    cell.className = "cell"
-    cell.innerText = i + 1
-    //aggiungere in pagina
-    grid.appendChild(cell)
-}
+button.addEventListener("click", function () {
+    for (let i = 0; i < totalCells; i++) {
+        //cella
+        const cell = document.createElement("div")
+        cell.className = "cell"
+        cell.innerText = i + 1
+        //aggiungere in pagina
+        grid.appendChild(cell)
+
+        cell.addEventListener("click", function () {
+            cell.classList.toggle("clicked")
+        })
+    }
+
+
+
+})
