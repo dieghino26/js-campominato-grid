a0a79e4645813c9ef02b85d6554aef26fa0f876c
dieghino26 <brotzadiego@gmail.com>
2024-01-11 10:06:52 +0100

    added reset grid

--- js/campominato.js
+++ js/campominato.js
@@ -9,30 +9,27 @@ const rows = 10
 const cols = 10
 const totalCells = rows * cols
 
-let createGrid = false
 
 button.addEventListener("click", function () {
 
-    if (!createGrid) {
-        for (let i = 0; i < totalCells; i++) {
-            //cella
-            const cell = document.createElement("div")
-            cell.className = "cell"
-            cell.innerText = i + 1
-            //aggiungere in pagina
-            grid.appendChild(cell)
+    button.innerText = "Ricomincia"
+    grid.innerHTML = ""
 
-            cell.addEventListener("click", function () {
-                cell.classList.toggle("clicked")
-                console.log(i + 1)
-            })
-        }
-        createGrid = true
 
+    //cell generator
+    for (let i = 0; i < totalCells; i++) {
+        //cella
+        const cell = document.createElement("div")
+        cell.className = "cell"
+        cell.innerText = i + 1
+        //aggiungere in pagina
+        grid.appendChild(cell)
+
+        cell.addEventListener("click", function () {
+            if (cell.classList.contains("clicked")) return
+            cell.classList.add("clicked")
+            console.log(i + 1)
+        })
     }
-
-
-
-
 
 })
